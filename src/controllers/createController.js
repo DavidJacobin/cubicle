aa229ce39735b7205bba5118a0689a8654792d27
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2022-12-16 16:24:45 +0200

    Create page works with data and saves new cubes

--- src/controllers/createController.js
+++ src/controllers/createController.js
@@ -1,8 +1,27 @@
 const router = require("express").Router();
+const cubes = require("../db.json")
 
+const fs = require("fs/promises")
 
 router.get('/create', (req, res) => {
     res.render("create")
+})
+
+router.post("/create", (req, res) =>{
+    const cube = req.body
+
+    if(cube.name.length == 0){
+        alert("Name must be filled!")
+        return
+    }
+
+    cubes.push(cube)
+    fs.writeFile("../db.json",JSON.stringify(cube))
+        .then(
+            res.redirect("/")
+        )
+
+
 })
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const app = express()
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }))
+
 app.engine("hbs", handlebars.engine({
     extname: "hbs"
 }));
